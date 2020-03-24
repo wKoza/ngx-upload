5faf531e548f303da3c9b1825ad2a4055002c794
William Koza <william.koza@gmail.com>
2020-03-24 13:30:33 +0100

    Upgrade angular 9 (#79)
    * upgrade angular 9

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,9 +2,12 @@
 
 All notable changes to this project will be documented in this file. See [standard-version](https://github.com/conventional-changelog/standard-version) for commit guidelines.
 
+
+<a name="9.0.0-beta.0"></a>
+# [9.0.0-beta.0](https://github.com/wKoza/ngx-upload/compare/v8.1.1...v9.0.0-beta.0) (2020-03-21)
+
 <a name="8.2.0"></a>
 # [8.2.0](https://github.com/wKoza/ngx-upload/compare/v8.1.1...v8.2.0) (2020-03-24)
-
 
 
 <a name="8.1.1"></a>
--- karma-shim.ts
+++ karma-shim.ts
@@ -1,14 +1,7 @@
-import 'core-js/client/shim';
-import 'reflect-metadata';
-
+import 'core-js';
 import 'zone.js/dist/zone';
-import 'zone.js/dist/long-stack-trace-zone';
-import 'zone.js/dist/proxy';
-import 'zone.js/dist/sync-test';
-import 'zone.js/dist/jasmine-patch';
-import 'zone.js/dist/async-test';
-import 'zone.js/dist/fake-async-test';
-import 'rxjs';
+import 'zone.js/dist/zone-testing';
+
 import { TestBed } from '@angular/core/testing';
 import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
 
