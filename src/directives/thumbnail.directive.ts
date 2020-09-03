e2f688fa660b9899002a51ff085556ad48541e19
William Koza <william.koza@gmail.com>
2020-09-03 18:01:16 +0200

    fix: EXIF thumbnail

--- src/directives/thumbnail.directive.ts
+++ src/directives/thumbnail.directive.ts
@@ -40,43 +40,48 @@ export class NgxThumbnailDirective implements OnInit {
                             ctx = canvas.getContext('2d')!;
 
                         // set proper canvas dimensions before transform & export
-                        if (4 < srcOrientation && srcOrientation < 9) {
-                            canvas.width = height;
-                            canvas.height = width;
-                        } else {
+                     //   if (4 < srcOrientation && srcOrientation < 9) {
+                     //       canvas.width = height;
+                       //     canvas.height = width;
+                    /*    } else {
                             canvas.width = width;
                             canvas.height = height;
-                        }
+                        }*/
+
+                        canvas.width = width;
+                        canvas.height = height;
+
+                        // draw image
+                        ctx.drawImage(img, 0, 0);
 
                         // transform context before drawing image
                         switch (srcOrientation) {
                             case 2:
-                                ctx.transform(-1, 0, 0, 1, width, 0);
+                                ctx.transform(-1, 0, 0, 1, canvas.width, 0);
                                 break;
                             case 3:
-                                ctx.transform(-1, 0, 0, -1, width, height);
+                                ctx.transform(-1, 0, 0, -1, canvas.width, canvas.height);
                                 break;
                             case 4:
-                                ctx.transform(1, 0, 0, -1, 0, height);
+                                ctx.transform(1, 0, 0, -1, 0, canvas.height);
                                 break;
                             case 5:
                                 ctx.transform(0, 1, 1, 0, 0, 0);
                                 break;
                             case 6:
-                                ctx.transform(0, 1, -1, 0, height, 0);
+                                ctx.transform(0, 1, -1, 0, canvas.height, 0);
                                 break;
                             case 7:
-                                ctx.transform(0, -1, -1, 0, height, width);
+                                ctx.transform(0, -1, -1, 0, canvas.height, canvas.width);
                                 break;
                             case 8:
-                                ctx.transform(0, -1, 1, 0, 0, width);
+                                ctx.transform(0, -1, 1, 0, 0, canvas.width);
                                 break;
                             default:
                                 break;
                         }
 
-                        // draw image
-                        ctx.drawImage(img, 0, 0);
+
 
                         this.renderer.setProperty(imgEl, 'src', canvas.toDataURL());
                     };
