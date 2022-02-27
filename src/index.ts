d610b95241c7123b6d230a4fc5016a2cf5799479
William Koza <william.koza@gmail.com>
2022-02-27 17:25:35 +0100

    tech: upgrade angular 13

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,7 @@ export { MineTypeEnum } from './utils/mimetype.model';
 export { FileItem } from './services/fileItem.model';
 export { HttpClientUploadService } from './services/httpClientUpload.service';
 export { NgxUploadModule } from './ngx-upload.module';
+export { NgxDragAndDropDirective} from './directives/dropzone.directive';
+export { NgxThumbnailDirective } from './directives/thumbnail.directive';
+export { NgxInputFileDirective } from './directives/inputfile.directive';
+export { InputfileComponent } from './components/inputfile.component';
